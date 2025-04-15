395a62181897177a92fad039c216e58cad715579
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2025-04-15 17:55:49 -0400

    Ya realiza la busqueda mediante la api de los libros perfectamente

--- app/src/main/java/com/ucb/ucbtest/navigation/AppNavigation.kt
+++ app/src/main/java/com/ucb/ucbtest/navigation/AppNavigation.kt
@@ -6,7 +6,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.ucb.ucbtest.finance.FinanceUI
+import com.ucb.ucbtest.book.BookUI
+import com.ucb.ucbtest.book.BookDetailUI
+import com.ucb.domain.Book
 
 @Composable
 fun AppNavigation() {
@@ -14,14 +16,30 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = Screen.FinanceScreen.route,
+        startDestination = Screen.BookSearchScreen.route,
         enterTransition = { EnterTransition.None },
         exitTransition = { ExitTransition.None },
         popEnterTransition = { EnterTransition.None },
         popExitTransition = { ExitTransition.None }
     ) {
-        composable(Screen.FinanceScreen.route) {
-            FinanceUI()
+        composable(Screen.BookSearchScreen.route) {
+            BookUI(
+                onBookSelected = { book ->
+                    navController.currentBackStackEntry?.savedStateHandle?.set("book", book)
+                    navController.navigate(Screen.BookDetailScreen.route)
+                }
+            )
+        }
+
+        composable(Screen.BookDetailScreen.route) {
+            val book = navController.previousBackStackEntry?.savedStateHandle?.get<Book>("book")
+            if (book != null) {
+                BookDetailUI(
+                    book = book,
+                    onBackPressed = { navController.popBackStack() }
+                )
+            }
         }
     }
 }
+
